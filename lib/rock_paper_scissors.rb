6e1985552bf05d712130a9982ad97def7b62934a
Sean Phillips <seanphillips9696@gmail.com>
2021-09-22 10:06:23 +0200

    checking for valid input and quit game

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,14 +1,21 @@
 class RockPaperScissors
 
-  def initialize
-
-    user_wins = 0
-    computer_wins = 0
-    options = ['rock', 'paper', 'scissors']
+  def initialize  
+    @user_wins = 0
+    @computer_wins = 0
+    @options = ['rock', 'paper', 'scissors']
   end
 
-  def play_game
+  def play_game 
+
+    loop {
+    user_input = user_choice
+    break if end_game?(user_input)
+    next if valid_input?(user_choice)
+
     
+    }
+
   end
 
   def get_input
@@ -21,6 +28,19 @@ class RockPaperScissors
 
   def computer_choice
   
+  end
+
+  def user_choice
+    puts 'Rock paper or scissors?'
+    user_input = gets.chomp.downcase
+  end
+
+  def end_game?(input)
+    input == 'q'
+  end
+
+  def valid_input?(input)
+    @options.include?(input)
   end
 
 end
